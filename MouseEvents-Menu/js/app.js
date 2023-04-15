47723f1643a2f470ca70c622e7ce21d77563bc0f
natalyq1 <onatylili@gmail.com>
2023-04-15 16:53:47 -0500

    mouse events, display none, block

---
+++ MouseEvents-Menu/js/app.js
@@ -0,0 +1,31 @@
+const boton = document.getElementById("boton");
+const indicador = document.getElementById("indicador");
+const mouseClickHandler = () => {
+  indicador.classList.add("click");
+  indicador.classList.remove("over");
+  indicador.classList.remove("out");
+};
+const mouseOverHandler = () => {
+  indicador.classList.add("over");
+  indicador.classList.remove("click");
+  indicador.classList.remove("out");
+};
+const mouseOutHandler = () => {
+  indicador.classList.add("out");
+  indicador.classList.remove("over");
+  indicador.classList.remove("click");
+};
+boton.addEventListener("click", mouseClickHandler);
+boton.addEventListener("mouseover", mouseOverHandler);
+boton.addEventListener("mouseout", mouseOutHandler);
+
+///menu 
+const linkMenu = document.getElementById("linkMenu");
+const menu = document.getElementById("menu");
+
+linkMenu.addEventListener("mouseover", () => {
+  menu.style.display = "block";
+});
+linkMenu.addEventListener("mouseout", () => {
+  menu.style.display = "none";
+});
