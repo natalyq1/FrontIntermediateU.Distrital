b800a45d8987ee6e9a225ca91a6c8effc1918cdc
natalyq1 <onatylili@gmail.com>
2023-04-14 22:55:46 -0500

    events, event listener, asociation of an event to an element. All onclick over text

---
+++ Eventos/js/app.js
@@ -0,0 +1,40 @@
+function alerta() {
+    alert("Hello");
+  }
+  
+  // Obtiene el elemento HTML
+  const tituloJavascript = document.getElementById("tituloJavascript");
+  
+  function mostrarFecha() {
+    const fechaActual = new Date();
+    console.log(fechaActual);
+  }
+  // Asocia un evento a un elemento NO SE ESCRIBEN LOS PARÉNTESIS DE LA FUNCIÓN*************************
+  tituloJavascript.onclick = mostrarFecha;
+  
+  // Obtiene una referencia al elemento
+  const tituloFecha = document.getElementById("tituloFecha");
+  
+  // El elemento escucha el evento click
+  tituloFecha.addEventListener("click", mostrarFecha);
+
+  const tituloFecha2 = document.getElementById("tituloFecha2");
+  tituloFecha2.addEventListener("click", function () {
+    console.log("Hello 1");
+  });
+
+  // Función anónima con arrow function
+  tituloFecha.addEventListener("click", () => {
+    // Obtiene la fecha actual
+    const fecha = new Date();
+    // Obtiene el día actual
+    const day = fecha.getDate();
+    // Muestra el día actual
+    tituloFecha.textContent = day;
+    // Verifica si el dia de hoy está en la primera o segunda quincena
+    if (day >= 1 && day <= 15) {
+      tituloFecha.classList.add("primera-quincena");
+    } else {
+      tituloFecha.classList.add("segunda-quincena");
+    }
+  });
