c409f72b564557f817ce98394f29c53d136ab9d6
natalyq1 <onatylili@gmail.com>
2023-05-12 19:45:54 -0500

    Style: inline and with an object json

--- React-primeros-pasos/react-admin-tareas/src/Components/Tareas/Tareas.jsx
+++ React-primeros-pasos/react-admin-tareas/src/Components/Tareas/Tareas.jsx
@@ -6,12 +6,25 @@ const Tareas = () => {
         { id: 3, titulo: 'Reading' },
         { id: 4, titulo: 'Writing' },
     ]
+    //OBJETO JSON PARA REPRESENTAR UN STYLE
+//objeto para el estilo, no requiere doble corchete al llamarlo
+const estiloSpan = {
+    color: 'lime',
+    fontStyle: 'italic'
+}
+
+
 //{Expresion}
     return (
         <>
+        {/* comentario con ctrl + k + c */}
+
+        {/* OBJETOS CON ESTILO INLINE */}
+        <h2 style={{color :'red', background:'pink'}}>Listado de tareas</h2>
+        <span style={estiloSpan}>List of tasks</span>
         {listaTareas.length > 0 ? ( 
-        <h2>Hay {listaTareas.length} tareas</h2>) :
-         (<h2>No hay tareas</h2>) }
+        <h3>Hay {listaTareas.length} tareas</h3>) :
+         (<h3>No hay tareas</h3>) }
             <ul>
                 {listaTareas.map(tarea => (
                     <li className="tarea" key={tarea.id}> {tarea.titulo}</li>
