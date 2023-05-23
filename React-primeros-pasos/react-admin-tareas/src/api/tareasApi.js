507654133d1bd4af05d5cb26c0a4202c8938ab48
natalyq1 <onatylili@gmail.com>
2023-05-22 22:21:23 -0500

    Setting up a .env , using variables of the envioroment with VITE

--- React-primeros-pasos/react-admin-tareas/src/api/tareasApi.js
+++ React-primeros-pasos/react-admin-tareas/src/api/tareasApi.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 const obtenerTareasAPI = async () => {
     try {
-        const respuesta = await axios.get('http://localhost:3000/tareas')
+        const respuesta = await axios.get(import.meta.env.VITE_BACKEND_URL)
         if (respuesta.status === 200) {
             return respuesta.data
         }
@@ -17,10 +17,15 @@ const obtenerTareasAPI = async () => {
         return null
     }
 }
+/**
+ * crea una tarea en el backend
+ * @param {*} tarea tarea a crear
+ * @returns Tarea creada
+ */
 
 const agregarTareaAPI = async (tarea) => {
     try {
-        const respuesta = await axios.post('http://localhost:3000/tareas', tarea)
+        const respuesta = await axios.post(import.meta.env.VITE_BACKEND_URL, tarea)
         if (respuesta.status === 201) {
             return respuesta.data
         } else {
