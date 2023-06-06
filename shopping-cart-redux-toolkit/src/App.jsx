4a2de10427331a5cc7c8cdb42519ff905ada07e8
natalyq1 <onatylili@gmail.com>
2023-06-06 18:17:28 -0500

    Redux: Reducer- calculating total

--- shopping-cart-redux-toolkit/src/App.jsx
+++ shopping-cart-redux-toolkit/src/App.jsx
@@ -1,8 +1,20 @@
+import {  useDispatch, useSelector } from "react-redux"
 import CartContainer from "./components/cart/CartContainer"
 import NavBar from "./components/navbar/NavBar"
+import { useEffect } from "react"
+import { calculateTotals } from "./features/cart/cartSlice"
 
 const App = () => {
-  
+  //Obtiene acceso a los items del cart
+  const { cartItems } = useSelector((store) => store.cart)
+
+  const dispatch = useDispatch()
+
+useEffect(() => {
+  // Recalcula totales cada vez que cambia cartItems
+  dispatch(calculateTotals())
+},[cartItems, dispatch])
+
   return (
     <>
       <NavBar />
--- shopping-cart-redux-toolkit/src/features/cart/cartSlice.js
+++ shopping-cart-redux-toolkit/src/features/cart/cartSlice.js
@@ -4,7 +4,7 @@ import cartItems from '../../data/cartItems';
 // Estado inicial
 const initialState = {
     cartItems,
-    amount: 4,
+    amount: 0,
     total: 0,
 }
 
@@ -36,8 +36,20 @@ const cartSlice = createSlice({
             const item = state.cartItems.find((item) => item.id === itemId)
             //decrementa la cantidad
             item.amount--
+        },
+        calculateTotals: (state) => {
+            let totalAmount = 0
+            let totalPrice = 0
+            //Recorre los items del cart
+            state.cartItems.forEach((item) => {
+                totalAmount += item.amount
+                totalPrice += item.amount * item.price
+            })
+            //Modifica el stado
+            state.amount = totalAmount
+            state.total = totalPrice
         }
     }
 })
-export const { clearCart, removeItem, increaseItemAmount, decreaseItemAmount } = cartSlice.actions
+export const { clearCart, removeItem, increaseItemAmount, decreaseItemAmount, calculateTotals } = cartSlice.actions
 export default cartSlice.reducer
