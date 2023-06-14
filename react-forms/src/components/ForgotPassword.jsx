b6b5dc5f50bc3d204ea8e7f1c24d669db0fb4437
natalyq1 <onatylili@gmail.com>
2023-06-14 18:06:21 -0500

    React Forms: Validating form Forgot Password with Formik(HOOKS)  and YUP

--- react-forms/src/components/ForgotPassword.jsx
+++ react-forms/src/components/ForgotPassword.jsx
@@ -1,21 +1,61 @@
 import { useContext } from "react";
+import { useFormik } from "formik";
 import SignContext from "../context/SignContext";
+import { forgotPasswordSchema } from "../schemas/forgotPasswordSchema";
 
 const ForgotPassword = () => {
 
   const { setStep } = useContext(SignContext)
 
+  const onSubmit = async (values, actions) => {
+    console.log(values);
+    console.log(actions);
+    //simula el envio de datos a un backend
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    //tiene acceso a los datos del formulario
+    console.log('%cError!', 'color:YellowGreen', `Password sent to ${values.email}!`
+    );
+    actions.resetForm()
+  }
+
+  //Limpia el formulario
+  
+
+  //Hook useFormik que permite manejar la info. del form
+  const { values, errors, handleChange, handleBlur, handleSubmit, isSubmitting } = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: forgotPasswordSchema,
+    onSubmit
+  })
+
   return (
     <main>
       <h3>Forgot password?</h3>
       <div className="card">
-        <form autoComplete="off">
+        <form
+          onSubmit={handleSubmit}
+          autoComplete="off">
           <fieldset>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" autoFocus />
-            <p className="error">Valid email required</p>
+            <input
+              type="email"
+              id="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              autoFocus
+              className={errors.email ? "error-forgot-input" : ''}
+            />
+            {errors.email && (
+              <p className="error-message">{errors.email}</p>)}
           </fieldset>
-          <button type="submit">Remember me!</button>
+          {isSubmitting && <small>Sending...</small>}
+          <button
+            disabled={isSubmitting}
+            type="submit">Remember me!</button>
         </form>
       </div>
       <p>
