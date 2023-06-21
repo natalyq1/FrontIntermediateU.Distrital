d4dea08f505dd8164971f98455a722febfccecd4
natalyq1 <onatylili@gmail.com>
2023-06-20 19:58:04 -0500

    Storybook: Component Boton with arguments

---
+++ Storybook/storybook-react/src/stories/Boton/Boton.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const Boton = ({label}) => {
+  return (
+    <button type="button">{ label }</button>
+  )
+}
+
+Boton.propTypes = {
+    label: PropTypes.string,
+}
+
+Boton.defaultProps = {
+    label: 'Botón',
+}
---
+++ Storybook/storybook-react/src/stories/Boton/Boton.stories.js
@@ -0,0 +1,18 @@
+import { Boton }from "./Boton";
+
+export default {
+    component: Boton
+}
+
+//stories
+export const Basic = {
+    args:{
+        label: 'Basic',
+    }
+}
+
+export const Advanced = {
+    args: {
+        label: 'Advanced',
+    }
+}
