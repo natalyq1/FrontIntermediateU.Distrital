68a4bbacc8b8966895115268c3ed19ef9b7491cb
natalyq1 <onatylili@gmail.com>
2023-06-23 19:17:11 -0500

    Storybook: Adding to add-on differents viewports, cutomized viewport

--- Storybook/storybook-react/.storybook/preview.js
+++ Storybook/storybook-react/.storybook/preview.js
@@ -1,4 +1,25 @@
 /** @type { import('@storybook/react').Preview } */
+
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+const customViewports = {
+  laptop: {
+    name: 'Laptop',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    }
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1025px',
+      height: '800px',
+    }
+
+  }
+}
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,10 +31,17 @@ const preview = {
     },
     options: {
       storySort: {
-        order: [ 'Atoms', 'Molecules', 'Example']
+        order: ['Atoms', 'Molecules', 'Example']
+      }
+    },
+    viewport: {
+      viewports: {
+        ...customViewports,
+        ...INITIAL_VIEWPORTS,
       }
     }
-  },
-};
+  }
+}
+
 
 export default preview;
