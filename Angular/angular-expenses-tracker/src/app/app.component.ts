a63c382799183ebef86c2d9ad65d6c1cb4bd2367
natalyq1 <onatylili@gmail.com>
2023-06-28 19:24:43 -0500

    Angular: ngFor to do a cycle on transactions

--- Angular/angular-expenses-tracker/src/app/app.component.ts
+++ Angular/angular-expenses-tracker/src/app/app.component.ts
@@ -1,15 +1,43 @@
+import { Transaction } from './models/transaction.model';
 import { Component } from '@angular/core';
 import { Balance } from './models/balance.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   balance: Balance = {
     amount: 100_000,
     income: 150_000,
     expenses: 50_000,
-  }
+  };
+  transactions: Transaction[] = [
+    {
+      type: 'expense',
+      amount: 45,
+      category: 'food',
+      date: new Date(2023, 6, 26),
+    },
+    {
+      type: 'expense',
+      amount: 200,
+      category: 'shopping',
+      date: new Date(2023, 6, 24),
+    },
+    {
+      type: 'expense',
+      amount: 60,
+      category: 'entertainment',
+      date: new Date(2023, 6, 22),
+    },
+    {
+      type: 'income',
+      amount: 500,
+      category: 'payroll',
+      date: new Date(2023, 6, 20),
+    },
+  ];
 }
+
