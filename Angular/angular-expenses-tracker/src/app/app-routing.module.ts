2f6816cce17a1bed857dc1d22f63102970dd9715
natalyq1 <onatylili@gmail.com>
2023-07-03 19:29:16 -0500

    Angular: Routing and refactoring (home component)

--- Angular/angular-expenses-tracker/src/app/app-routing.module.ts
+++ Angular/angular-expenses-tracker/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddTransactionComponent } from './components/add-transaction/add-transaction.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'add', component: AddTransactionComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
