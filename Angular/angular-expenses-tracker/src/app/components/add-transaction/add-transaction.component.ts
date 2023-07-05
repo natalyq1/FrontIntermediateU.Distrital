d39d0c536f4fa37caee782166bb9cf51353df187
natalyq1 <onatylili@gmail.com>
2023-07-04 20:09:21 -0500

    Angular: Reactive form on add-transaction-component

--- Angular/angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
+++ Angular/angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -9,9 +10,15 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AddTransactionComponent implements OnInit {
   addTransactionForm!: FormGroup;
   ngOnInit(): void {
+    const date: string = new Date().toISOString().split('T')[0];
+    //const date: string = formatDate(new Date(2022, 7, 30), 'yyyy-MM-dd', 'en')
+    
     //Hook que se ejecuta al montar el componente en el DOM
     this.addTransactionForm = new FormGroup({
-      amount: new FormControl(0),
+      amount: new FormControl(),
+      type: new FormControl('expense'),
+      category: new FormControl('food'),
+      date: new FormControl(date),
     });
   }
 }
