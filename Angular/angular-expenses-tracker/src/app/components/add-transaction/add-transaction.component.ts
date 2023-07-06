388dd763c4d383327decc46371c9566debd07ee5
natalyq1 <onatylili@gmail.com>
2023-07-05 19:04:30 -0500

    Angular: Form validation and showing error message

--- Angular/angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
+++ Angular/angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
@@ -1,6 +1,6 @@
 import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-transaction',
@@ -12,13 +12,24 @@ export class AddTransactionComponent implements OnInit {
   ngOnInit(): void {
     const date: string = new Date().toISOString().split('T')[0];
     //const date: string = formatDate(new Date(2022, 7, 30), 'yyyy-MM-dd', 'en')
-    
+
     //Hook que se ejecuta al montar el componente en el DOM
     this.addTransactionForm = new FormGroup({
-      amount: new FormControl(),
+      amount: new FormControl(null, Validators.required),
       type: new FormControl('expense'),
       category: new FormControl('food'),
       date: new FormControl(date),
     });
+  }
+
+  onSubmit() {
+    if (this.addTransactionForm.valid) {
+      console.log(this.addTransactionForm);
+      console.log(this.addTransactionForm.value);
+      //this.addTransactionForm.reset();
+      alert('Everything is ok');
+    } else {
+      console.error('The form is not valid');
+    }
   }
 }
