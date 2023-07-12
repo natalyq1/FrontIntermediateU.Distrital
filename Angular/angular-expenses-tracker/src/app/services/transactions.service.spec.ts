938b7b59381648756795bd66929483b8ded9e505
natalyq1 <onatylili@gmail.com>
2023-07-12 18:33:30 -0500

    Angular: Unit testing on a service, asyncronous

--- Angular/angular-expenses-tracker/src/app/services/transactions.service.spec.ts
+++ Angular/angular-expenses-tracker/src/app/services/transactions.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransactionsService } from './transactions.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Transaction } from '../models/transaction.model';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule]
+    });
     service = TestBed.inject(TransactionsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all transactions', (done) => {
+    service.getTransactions().subscribe((transactions: Transaction[]) => {
+      //console.log(transactions);
+      expect(transactions.length).toBe(7)
+      done()
+    })
+  });
+
 });
