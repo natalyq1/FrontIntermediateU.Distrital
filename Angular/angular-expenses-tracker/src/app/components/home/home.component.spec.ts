4825447753ecf852e1d4c3c224133c4ddae39812
natalyq1 <onatylili@gmail.com>
2023-07-12 19:50:05 -0500

    Angular: Unit testing- Created an html test for HomeComponent

--- Angular/angular-expenses-tracker/src/app/components/home/home.component.spec.ts
+++ Angular/angular-expenses-tracker/src/app/components/home/home.component.spec.ts
@@ -8,18 +8,26 @@ import { TransactionsComponent } from '../transactions/transactions.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [HomeComponent, BalanceComponent, TransactionsComponent]
+      declarations: [HomeComponent, BalanceComponent, TransactionsComponent],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    //console.log(compiled); //<div id="root0" _nghost-a-c3559178710="" ng-version="16.1.2"><div _ngcontent-a-c3559178710="" class="main"...
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a div with css class called main', () => {
+    const div = compiled.querySelector('.main')
+    expect(div).toBeTruthy()
   });
 });
