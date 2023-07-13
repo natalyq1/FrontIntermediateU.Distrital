a1f8072f298685558da3d3f8aa6b0094ca0015e2
natalyq1 <onatylili@gmail.com>
2023-07-12 19:03:22 -0500

    Angular: Unit testing, getting the right type of a transaction

--- Angular/angular-expenses-tracker/src/app/services/transactions.service.spec.ts
+++ Angular/angular-expenses-tracker/src/app/services/transactions.service.spec.ts
@@ -9,7 +9,7 @@ describe('TransactionsService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule]
+      imports: [HttpClientModule],
     });
     service = TestBed.inject(TransactionsService);
   });
@@ -21,9 +21,16 @@ describe('TransactionsService', () => {
   it('should get all transactions', (done) => {
     service.getTransactions().subscribe((transactions: Transaction[]) => {
       //console.log(transactions);
-      expect(transactions.length).toBe(7)
-      done()
-    })
+      expect(transactions.length).toBe(7);
+      done();
+    });
   });
 
+  it('should get the right type of a transaction', (done) => {
+    service.getTransactions().subscribe((transactions: Transaction[]) => {
+      const transaction: Transaction = transactions[3];
+      expect(transaction.type).toBe('expense');
+      done();
+    });
+  });
 });
